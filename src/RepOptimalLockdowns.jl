973a72477f18955a1d7b767e3eee909101c33320
燕靖明 <144886569+iezm@users.noreply.github.com>
2025-01-01 23:47:06 +0100

    Initialize the package

---
+++ src/RepOptimalLockdowns.jl
@@ -0,0 +1,10 @@
+module RepOptimalLockdowns
+
+# Calling external packages
+
+# export run
+function run()
+    return 1 + 1
+end
+
+end # module RepOptimalLockdowns
---
+++ test/runtests.jl
@@ -0,0 +1,3 @@
+using Test, RepOptimalLockdowns
+
+@test RepOptimalLockdowns.run() == 2
